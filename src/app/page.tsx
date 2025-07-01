0531125bcba24131f357262233e45a7e91d0ee8c
M.Robi <mrobichaniago@gmail.com>
2025-07-01 17:28:12 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import HomeLayout from "@/components/HomeLayout";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const PageContainer = dynamic(() => import("@/components/container/PageContainer"), {
   ssr: false,
 });
 
+const HomeLayout = dynamic(() => import("@/components/HomeLayout"), {
+  ssr: false,
+});
+
 export default function Page() {
   return (
-    <PageContainer>
-      <HomeLayout />
-    </PageContainer>
+    <Suspense>
+      <PageContainer>
+        <HomeLayout />
+      </PageContainer>
+    </Suspense>
   );
 }
